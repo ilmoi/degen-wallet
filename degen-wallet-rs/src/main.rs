fb2758467782f7e2686d9c996f5a8c3ca60cf90b
ilmoi <iljamoi@protonmail.com>
2021-07-22 20:01:27 +0300

    polish

--- degen-wallet-rs/src/main.rs
+++ degen-wallet-rs/src/main.rs
@@ -26,37 +26,4 @@ fn panic_hook(info: &PanicInfo<'_>) {
 fn main() {
     panic::set_hook(Box::new(panic_hook));
     draw_screen().unwrap();
-
-    // // ----------------------------------------------------------------------------- addresses
-    // let a1 = H160::from_str("0xCd550E94040cEC1b33589eB99B0E1241Baa75D19").unwrap();
-    // let a2 = H160::from_str("0xC48ad5fd060e1400a41bcf51db755251AD5A2475").unwrap();
-    // println!("{:?}, {:?}", a1, a2);
-
-    // // ----------------------------------------------------------------------------- eth wallet
-    // let mnemonic = "window recall kid brief dragon worry intact board thumb aunt hair cement";
-    // let mnemonic = mnemonic_from_phrase(mnemonic).unwrap();
-    // let (a, pubk, prvk) = generate_eth_wallet(&mnemonic);
-
-    // ----------------------------------------------------------------------------- ctr
-    // query_contracts(&vec![a1, a2]);
-
-    // let r = transfer_contract_public("uni", prvk[0], a2, 0.01);
-
-    // ----------------------------------------------------------------------------- test sol tx
-
-    // let alice = Pubkey::from_str("Ga8HG4NzgcYkegLoJDmxJemEU1brewF2XZLNHd6B4wJ7").unwrap();
-    // let bob = Pubkey::from_str("BxiV2mYXbBma1Kv7kxnn7cdM93oFHL4BhT9G23hiFfUP").unwrap();
-    // let nob = Pubkey::from_str("C8xgzKnCAQbvQ5dEDtZmWfTbWJAzVcW91WXiu35aqjAH").unwrap();
-    //
-    // let secret = &[
-    //     201, 101, 147, 128, 138, 189, 70, 190, 202, 49, 28, 26, 32, 21, 104, 185, 191, 41, 20, 171,
-    //     3, 144, 4, 26, 169, 73, 180, 171, 71, 22, 48, 135, 231, 91, 179, 215, 3, 117, 187, 183, 96,
-    //     74, 154, 155, 197, 243, 114, 104, 20, 123, 105, 47, 181, 123, 171, 133, 73, 181, 102, 41,
-    //     236, 78, 210, 176,
-    // ];
-    // let payer = solana_sdk::signer::keypair::Keypair::from_bytes(secret).unwrap();
-    //
-    // // query_program("t1", alice).unwrap();
-    // // transfer_spl_token("t1", &payer, bob, 1.0).unwrap();
-    // send_sol(&bob, 0.1, &payer).unwrap();
 }
